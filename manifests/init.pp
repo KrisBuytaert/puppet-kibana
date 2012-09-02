413dd6b2e4443ff429317786bbd728217adaf36f
Kris Buytaert <sdog@inuits.be>
2012-09-02 19:20:59 +0200

    Split of vhost config

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,14 +18,7 @@ class kibana($elasticsearch_server ='localhost:9200')
     ensure => present;
   }
 
-  file { '/etc/httpd/conf.d/kibana.conf':
-    ensure => 'file',
-    group  => '0',
-    mode   => '0644',
-    owner  => '0',
-    source => 'puppet:///modules/kibana/kibana.conf',
-  }
-
+  
 
   file { '/var/vhosts/kibana/htdocs/config.php':
     ensure   => 'file',
@@ -36,3 +29,18 @@ class kibana($elasticsearch_server ='localhost:9200')
   }
 
 }
+
+# Use kibana::vhost if you don't manage apache already on this host (cfr
+# https://github.com/KrisBuytaert/vagrant-puppet-logstash/blob/master/manifests/classes/my-logstash.pp
+#
+class kibana::vhost{
+  file { '/etc/httpd/conf.d/kibana.conf':
+    ensure => 'file',
+    group  => '0',
+    mode   => '0644',
+    owner  => '0',
+    source => 'puppet:///modules/kibana/kibana.conf',
+  }
+
+}
+
