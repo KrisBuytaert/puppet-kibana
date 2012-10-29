7e9cd7d108e620f5ab9e281a248c373092945786
Kris Buytaert <sdog@inuits.be>
2012-10-29 16:20:51 +0100

    AutoLoadinLayout

--- manifests/init.pp
+++ manifests/init.pp
@@ -30,17 +30,3 @@ class kibana($elasticsearch_server ='localhost:9200')
 
 }
 
-# Use kibana::vhost if you don't manage apache already on this host (cfr
-# https://github.com/KrisBuytaert/vagrant-puppet-logstash/blob/master/manifests/classes/my-logstash.pp
-#
-class kibana::vhost{
-  file { '/etc/httpd/conf.d/kibana.conf':
-    ensure => 'file',
-    group  => '0',
-    mode   => '0644',
-    owner  => '0',
-    source => 'puppet:///modules/kibana/kibana.conf',
-  }
-
-}
-
